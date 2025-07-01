29bec1bd03aa268bf382ce6ce3befc5558eb6c70
Josh Bello <Jb230913@live.com>
2025-07-01 11:17:40 +0100

    pages - update html tags to laravel components to implement DRY

--- resources/views/passwordCategories/newpasswordCategory.blade.php
+++ resources/views/passwordCategories/newpasswordCategory.blade.php
@@ -11,21 +11,19 @@
         <div class="col-xs-12
                 col-md-12
                 ">
-                <label for="exampleInputEmail1" class="form-label">Ttitle</label>
-                <input type="text" class="border-1 border-black border-solid"  id="title"  name="title" >
+                <x-form-label for="title" class="block text-sm font-medium text-gray-700">Title</x-form-label>
+                <x-form-field type="text" class="border-1 border-black border-solid"  id="title" name="title"  value="{{ old('title') }}"></x-form-field>  
                 
         </div>
-        <div class="col-xs-12
-                col-md-12
-                ">
-                <label for="password" class="form-label">Additional Notes</label>
-                <textarea class="border-1 border-black border-solid"   id="notes" name="notes" cols="10"></textarea>
+        <div class="col-xs-12 col-md-12">
+                <x-form-label class="form-label"  id="notes"  name="notes" > Additional Notes </x-form-label>
+                <x-form-textarea class="border-1 border-black border-solid" id="notes" name="notes" cols="10"> {{ old('notes') }}</x-form-textarea>
             
         </div> 
 
         <div class="col-xs-12 col-md-12">
-                <a href="/dashboard">Cancel</a>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <a href="/dashboard">Cancel</a>
+        <x-form-button class="btn btn-primary"  type="submit">Submit</x-form-field>
         </div>
         </form>
     </div>
