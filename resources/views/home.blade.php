8152a5a8ec8f9e94265d48aa49d21936b4cc1e87
Josh Bello <Jb230913@live.com>
2025-07-01 09:02:10 +0100

    home.blade.php - added BEM + html tag to login form element

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,27 +6,25 @@
 
 <div class="l-container">
     <div class="row center-md">
-    <form action="{{route('login.post')}}" method="POST" class=" mx-auto " style="width:500px;">
-    @csrf
-        <div class="col-xs-12
-                col-md-12
-                ">
-                <x-form-label for="email" class="block text-sm font-medium text-gray-700">Email</x-form-label>
-                {{-- <input type="email" class="border-1 border-black border-solid"  id="email"  name="email" > --}}
-                <x-form-field class="border-1 border-black border-solid"  type="text" id="email" name="email"></x-form-field>
+        <div class="c-login-form">
+            <form action="{{route('login.post')}}" method="POST" class=" mx-auto " style="width:500px;">
+            @csrf
+                <div class="col-xs-12 col-md-12">
+                        <x-form-label for="email" class="block text-sm font-medium text-gray-700">Email</x-form-label>
+                        <x-form-field class="border-1 border-black border-solid"  type="text" id="email" name="email"></x-form-field>
+                </div>
+                <div class="col-xs-12
+                        col-md-12
+                        ">
+                        <x-form-label for="password" class="block text-sm font-medium text-gray-700">Password</x-form-label>
+                        <x-form-field class="border-1 border-black border-solid"  type="password" id="password" name="password" ></x-form-field>
+                </div>
+        
+                <div class="col-xs-12 col-md-12">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
-        <div class="col-xs-12
-                col-md-12
-                ">
-                <x-form-label for="password" class="block text-sm font-medium text-gray-700">Password</x-form-label>
-                {{-- <input type="password" class="border-1 border-black border-solid"  id="password" name="password" > --}}
-                <x-form-field class="border-1 border-black border-solid"  type="password" id="password" name="password" ></x-form-field>
-        </div>
-
-        <div class="col-xs-12 col-md-12">
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-        </form>
     </div>
 </div>
 @endsection
