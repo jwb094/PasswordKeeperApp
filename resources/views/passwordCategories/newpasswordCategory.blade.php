d6101fe6e9a76f5e1f3376ab047e6003e17008fd
Josh Bello <Jb230913@live.com>
2025-08-11 19:10:34 +0100

    update - changed selector for note field

--- resources/views/passwordCategories/newpasswordCategory.blade.php
+++ resources/views/passwordCategories/newpasswordCategory.blade.php
@@ -6,26 +6,25 @@
 
 <div class="l-container">
     <div class="row center-md">
-        <div class="c-form-block">
-            <form action="{{ route('store.data') }}" method="POST">
+
+       <div class="c-form-block c-form-block--new-pwdcat | radius-lg border-black border-1">
+                <h1>New Category</h1>
+            <form action="{{ route('store.data') }}" method="POST" class=" w-full mx-auto my-56 ">
                 @csrf
-                <div class="col-xs-12
-                col-md-12
-                ">
-                    <label for="exampleInputEmail1" class="form-label">Ttitle</label>
-                    <input type="text" class="border-1 border-black border-solid" id="title" name="title">
-
+                <div class="col-xs-12 col-md-12 | my-24 ">
+                    <div class="entryarea">
+                        <input type="text" class="form-field border-1 border-black border-solid" id="title" name="title" required>
+                        <label for="title" class="form-label">Title</label>
+                    </div>
                 </div>
-                <div class="col-xs-12
-                col-md-12
-                ">
-                    <label for="password" class="form-label">Additional Notes</label>
-                    <textarea class="border-1 border-black border-solid" id="notes" name="notes" cols="10"></textarea>
-
+                <div class="col-xs-12 col-md-12 | my-48">
+                    <div class="entryarea">
+                        <textarea class="form-field border-1 border-black border-solid" id="notes" name="notes" rows="2" placeholder=" " required></textarea>
+                        <label for="notes" class="form-label">Additional Notes</label>
+                    </div>
                 </div>
-
-                <div class="col-xs-12 col-md-12">
-                    <a href="/dashboard">Cancel</a>
+                <div class="col-xs-12 col-md-12 | my-48 mx-auto flex justify-content-center flex-row  align-items-center gap-12">
+                    <a class="btn btn-secondary" href="/dashboard">Cancel</a>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </form>
